75afde62477e3a89782e292e8aa2f975b5aa57c3
Andrew Yakunin <NcQushka@gmail.com>
2024-02-11 22:07:30 +0300

    Mediatr and repository sample

--- VSOP.Application/Worlds/Queries/GetByIdWithCountries/GetWroldByIdWithCountriesQuery.cs
+++ VSOP.Application/Worlds/Queries/GetByIdWithCountries/GetWroldByIdWithCountriesQuery.cs
@@ -3,4 +3,4 @@ using VSOP.Domain.DbModels.Worlds;
 
 namespace VSOP.Application.Worlds.Queries.GetById;
 
-public sealed record GetWorldByIdQuery(Guid Id) : IQuery<World>;
+public sealed record GetWroldByIdWithCountriesQuery(Guid Id) : IQuery<World>;
---
+++ VSOP.Application/Worlds/Queries/GetByIdWithCountries/GetWroldByIdWithCountriesQueryValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace VSOP.Application.Worlds.Queries.GetById;
+
+internal class GetWroldByIdWithCountriesQueryValidator : AbstractValidator<GetWroldByIdWithCountriesQuery>
+{
+    public GetWroldByIdWithCountriesQueryValidator()
+    {
+        RuleFor(x => x.Id).NotNull();
+    } 
+}
