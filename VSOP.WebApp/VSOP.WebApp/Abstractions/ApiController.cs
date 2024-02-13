f17304138fa9cd6f0395d5caef1c9f0a2f68daef
Dmitry Egorov <egorov.dm.d@gmail.com>
2024-02-13 05:08:59 +0300

    SuperMinorChanges

--- VSOP.WebApp/VSOP.WebApp/Abstractions/ApiController.cs
+++ VSOP.WebApp/VSOP.WebApp/Abstractions/ApiController.cs
@@ -11,7 +11,7 @@ namespace VSOP.WebApp.Abstractions; //Пока поживет тут, потом
 /// Базовое представление ApiContoller-а
 /// </summary>
 [ApiController]
-public abstract class ApiController : ControllerBase //TODO: Разнести HTTP-коды
+public abstract class ApiController : ControllerBase
 {
     protected readonly ISender Sender;
     /// <summary>
@@ -68,7 +68,7 @@ public abstract class ApiController : ControllerBase //TODO: Разнести HT
         }
     }
 
-    static ProblemDetails CreateProblemDetails( //TODO: Изменить построение статусов
+    static ProblemDetails CreateProblemDetails(
         string title,
         int status,
         Error error,
